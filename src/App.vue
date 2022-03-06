5844ac48413d228068c09d13a3ab7a3de20f732c
Marc-Antoine Dumais <mdumais@expedia.com>
2022-03-06 17:41:48 -0500

    testing redeploy

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to FinanceViz, a tool to visualize financial data"/>
+  <HelloWorld msg="Welcome to FinanceViz, a tool to visualize financial data!"/>
 </template>
 
 <script lang="ts">
